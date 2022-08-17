a9722d6bb818c9e0f4cb86690bf3d7eded01ea55
Rob Thomure <robthomure@outlook.com>
2022-08-17 13:23:20 -0500

    Stage 1: outputs the ASCII picture of the cow line-by-line.

---
+++ src/ASCIICow.java
@@ -0,0 +1,16 @@
+
+public class ASCIICow {
+    private final static String COW =
+            "                    _______ \n" +
+                    "                   < hello >\n" +
+                    "                    ------- \n" +
+                    "            ^__^   /        \n" +
+                    "    _______/(oo)  /         \n" +
+                    "/\\/(       /(__)            \n" +
+                    "   | w----||                \n" +
+                    "   ||     ||                ";
+
+    public static void printASCIICow() {
+        System.out.println(COW);
+    }
+}

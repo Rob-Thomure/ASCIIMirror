354c811acad0883bff09f128c36f97128b819ca7
Rob Thomure <robthomure@outlook.com>
2022-08-21 14:52:51 -0500

    Stage 5: asks for text file path input. then prints that text file and mirror image to console.

---
+++ README.md
@@ -0,0 +1,38 @@
+# ASCII Mirror
+
+___
+
+A Jetbrains Academy project. Written using Java.
+
+The application reads a text file with an ASCII image and then outputs the ASCII image
+with a mirror image next to it.
+
+___
+
+
+## Learning Ouctomes
+
+* Java basics
+* input, output, strings, files, lists
+___
+
+## Instructions
+
+1. Upon running app. The user will be prompted to enter a file path in the console.
+
+        example input: ASCII_Animals/HumphFolder/Cow.txt
+
+    Camel.txt contents:
+    ![img.png](img.png)   
+
+
+2. The app will output the ASCII text image and a mirror ASCII text image to the right.
+
+    
+
+        Output example:
+
+![img_1.png](img_1.png)
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,97 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        Stage3.run();
+        System.out.println("Input the file path:");
+        Scanner scanner = new Scanner(System.in);
+        File file = new File(scanner.nextLine());
+        List<String> inputList = new ArrayList<>();
+        try {
+            scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                inputList.add(scanner.nextLine());
+            }
+            List<String> list = setLinesEqualSize(inputList);
+            List<String> modifiedList = reverseLines(list);
+            printLines(list, modifiedList);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+
+    }
+
+    public static void printLines(List<String> list, List<String> modifiedList) {
+        for (int i = 0; i < list.size(); i++) {
+            String line = list.get(i);
+            String modifiedLine = modifiedList.get(i);
+            System.out.println(line + " | " + modifiedLine);
+        }
+    }
+
+    public static int countLongestLine(List<String> list) {
+        int longestLineSize = 0;
+        for (String element : list) {
+            if (element.length() > longestLineSize) {
+                longestLineSize = element.length();
+            }
+        }
+        return longestLineSize;
+    }
+
+    public static List<String> setLinesEqualSize(List<String> list) {
+        int longestLineSize = countLongestLine(list);
+        List<String> modifiedList = new ArrayList<>();
+        for (String element : list) {
+            int spaces = longestLineSize - element.length();
+            modifiedList.add(element + " ".repeat(spaces));
+        }
+        return modifiedList;
+    }
+
+
+
+    public static List<String> reverseLines(List<String> list) {
+        List<String> modifiedList = new ArrayList<>();
+        for (String element : list) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < element.length(); i++) {
+                stringBuilder.append(reverseAsymmetricChar(element.charAt(i)));
+            }
+            modifiedList.add(stringBuilder.reverse().toString());
+        }
+        return modifiedList;
+    }
+
+    public static char reverseAsymmetricChar(char character) {
+        switch (character) {
+            case '<':
+                return '>';
+            case '>':
+                return '<';
+            case '[':
+                return ']';
+            case ']':
+                return '[';
+            case '{':
+                return '}';
+            case '}':
+                return '{';
+            case '(':
+                return ')';
+            case ')':
+                return '(';
+            case '/':
+                return '\\';
+            case '\\':
+                return '/';
+            default:
+                return character;
+
+        }
     }
 }
